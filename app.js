3df758017d99389cb983026841425dca7d3192ca
Ilham <Ilham>
2022-01-16 15:02:26 +0300

    last version

--- app.js
+++ app.js
@@ -1,64 +1,145 @@
-const formEl = document.getElementById('form');
-const mesEl = document.getElementById('mess');
-const tagEl = document.getElementById('tag');
-const linkEl = document.getElementById('link');
-const listEl = document.getElementById('list');
-let readEl = document.getElementById('read');
+const readEL = document.getElementById("home")
+const readenEl = document.getElementById("profile")
+const searchEl = document.getElementById("contact")
+const nameEl = document.getElementById("name")
+const tagEl = document.getElementById("tag")
+const linkEl = document.getElementById("link")
+const addEl = document.getElementById("add")
+const windowEl = document.getElementById("window")
 
+let list = []
 
+let readList = []
 
-class Task {
-    constructor(mes, tag, link) {
-        this.mes = mes;
-        this.tag = tag;
-        this.link = link;
-    }
+readEL.addEventListener('click', () => {
 
+    nameEl.style.display = 'inline'
 
-}
+    tagEl.style.display = 'inline'
 
-class TaskList {
-    constructor() {
-        this.list = []
+    linkEl.style.display = 'inline'
 
+    addEl.style.display='inline'
 
-
-    }
-
-    add(task) {
-        this.list.push(task);
-        this.render()
-        localStorage.setItem('key', JSON.stringify(this.list))
-    }
-
-    render() {
-        listEl.innerHTML = '';
-        for (const task of this.list) {
-            const el = document.createElement('li')
-            el.innerHTML = `
-            <div class="alert alert-success" role="alert">
-            <input type="checkbox" id="i-checkbox" value="selected">
-              ${task.mes}&nbsp${task.tag}&nbsp${task.link}
-            </div>
-            `
-            listEl.appendChild(el)
-        }
-
-
-    }
-}
-
-const store = new TaskList()
-
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const mes = mesEl.value;
-    const tag = tagEl.value;
-    const link = linkEl.value;
-    const task = new Task(mes, tag, link);
-    store.add(task)
-    console.log(store.list)
-
+    renderList()
 })
-const readen = localStorage.getItem('key')
-readEl.value = JSON.parse(readen)
+
+readenEl.addEventListener('click', () => {
+
+    nameEl.style.display = 'none'
+
+    tagEl.style.display = 'none'
+
+    linkEl.style.display = 'none'
+
+    addEl.style.display='none'
+
+    renderReadList()
+})
+
+class Read {
+    constructor(name, tag, link) {
+        this.name = name
+        this.tag = tag
+        this.link = link
+
+    }
+}
+
+
+addEl.addEventListener('click', () => {
+    const name = nameEl.value
+    const tag = tagEl.value
+    const link = linkEl.value
+    const item = new Read(name, tag, link)
+
+
+
+    list.push(item)
+
+    renderList()
+})
+
+const renderList = () => {
+    windowEl.innerHTML = ''
+
+    for (const item of list) {
+        const el = document.createElement('div')
+        const index = list.indexOf(item)
+
+        for (const index of list){
+            el.innerHTML = `
+        <div>
+        <input id="checkbox" type="checkbox">
+        <span class="badge bg-light text-dark">Название ${item.name}</span> <span class="badge bg-light text-dark">Тэг ${item.tag}</span> <span class="badge bg-light text-dark">Ссылка ${item.link}</span>
+        <button type="button" class="btn btn-outline-danger" id="del">Удалить</button>
+        
+        </div>`
+        }
+        windowEl.appendChild(el)
+
+        const delEl = document.querySelector('#del')
+
+        delEl.addEventListener('click', () => {
+
+
+            list.splice(list[index], 1)
+
+            renderList()
+        })
+
+        const checkEl = el.querySelector('#checkbox')
+
+        checkEl.addEventListener('change', () => {
+
+
+
+            list.splice(list[index], 1)
+
+            readList.push(list[index])
+
+            renderList()
+        })
+    }
+}
+
+const renderReadList = () => {
+    windowEl.innerHTML = ''
+
+    for (const item of readList) {
+        const el = document.createElement('div')
+        const readIndex = readList.indexOf(item)
+
+
+        el.innerHTML = `
+        <div>
+        <input id="checkbox" type="checkbox" checked>
+         <span class="badge bg-light text-dark">Название ${item.name}</span> <span class="badge bg-light text-dark">Тэг ${item.tag}</span> <span class="badge bg-light text-dark">Ссылка ${item.link}</span>
+        <button type="button" class="btn btn-outline-danger" id="del">Удалить</button>
+        
+        </div>`
+        windowEl.appendChild(el)
+
+        const delEl = document.getElementById('del')
+
+        delEl.addEventListener('click', () => {
+
+
+            readList.splice(readList[readIndex], 1)
+
+            renderReadList()
+        })
+
+        const checkEl = el.querySelector('#checkbox')
+
+        checkEl.addEventListener('change', () => {
+
+
+            readList.splice(readList[readIndex], 1)
+
+            list.push(readList[readIndex])
+
+            renderReadList()
+        })
+    }
+}
